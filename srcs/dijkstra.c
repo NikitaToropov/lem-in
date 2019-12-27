3df219ba7adfabb78f556f435c7d0a7dbb071546
NktTrpv <nikita_toropov@nkt.local>
2019-12-27 20:29:32 +0300

    its alive! alive!!!!!

--- srcs/dijkstra.c
+++ srcs/dijkstra.c
@@ -1,30 +1,39 @@
 #include <lem_in.h>
 
-int		*make_arr(t_matrix *matrix, int num_of_verts, int *p)
+int		*make_arr(t_matrix *matrix, int *p)
 {
 	int		*arr;
-	int		i;
+	int		len;
 	int		j;
+	int		i;
 
-	if (!(arr = malloc(sizeof(int) * (num_of_verts + 1))))
+	len = 1;
+	j = matrix->finish;
+	while (j != matrix->start)
+	{
+		j = p[j];
+		len++;
+		write(1, "THETRTETRT\n", 11);
+	}
+	if (!(arr = malloc(sizeof(int) * (len + 1))))
 		return (arr);
 
-	arr[0] = num_of_verts;
+	arr[0] = len;
 	arr[1] = matrix->start;
 	i = matrix->finish;
-	j = num_of_verts;
+	j = len;
 	while (i != matrix->start && j > 1)
 	{
 		arr[j--] = i;
 		i = p[i];
 	}
 	i = 0;
-	while (i <= num_of_verts)
+	while (i <= len)
 		printf(" %d", arr[i++]);
 	return (arr);
 }
 
-int		*dijkstra(t_matrix *matrix, int *parents)
+int		*dijkstra(t_matrix *matrix, int *children)
 {
 	int		d[matrix->len];
 	int		p[matrix->len];
@@ -32,42 +41,38 @@ int		*dijkstra(t_matrix *matrix, int *parents)
 	int		i = 0;
 	int		j = 0;
 
+	// children[matrix->start] = -1;	
 	while (i < matrix->len)
 	{
 		d[i] = INT_MAX;
 		u[i++] = 0;
+		p[j] = -1;
 	}
 	d[matrix->start] = 0;
 	j = matrix->start;
 	while (j != matrix->finish)
 	{
+			printf(" %d", j);
 		if (j > matrix->len || d[j] == INT_MAX)
 			return (NULL);
 		u[j] = 1;
-		if (parents[j] != -1 && matrix->mtrx[parents[j]][parents[parents[j]]])
+		i = 0;
+		while (i < matrix->len)
 		{
-			i = parents[j];
-			d[i] = matrix->mtrx[j][i] + d[j];
-			p[i] = j;
-			parents[parents[j]] = -1;
-		}
-		else
-		{
-			i = 0;
-			while (i < matrix->len)
+			if (matrix->mtrx[j][i] && d[i] > matrix->mtrx[j][i] + d[j])
 			{
-				if (matrix->mtrx[j][i] && d[i] > matrix->mtrx[j][i] + d[j])
-				{
-					d[i] = matrix->mtrx[j][i] + d[j];
-					p[i] = j;
-				}
-				i++;
+				d[i] = matrix->mtrx[j][i] + d[j];
+				p[i] = j;
 			}
+			i++;
 		}
+		if (children[j] != -1 && p[j] != -1 && children[p[j]] == -1)
+			p[children[j]] = j;
 		j = 0;
 		while (j < matrix->len && !(!u[j] && d[j] != INT_MAX))
 			j++;
 	}
+					printf("\n");
 	if (d[matrix->finish] == INT_MAX)
 		return (NULL);
 
@@ -76,7 +81,12 @@ int		*dijkstra(t_matrix *matrix, int *parents)
 	while (i < matrix->len)
 		printf(" %d", d[i++]);
 	printf("\n");
+	i = 0;
+	while (i < matrix->len)
+		printf(" %d", p[i++]);
+	printf("\n");
 
 
-	return (make_arr(matrix, d[matrix->finish] + 1, p));
+	return (make_arr(matrix, p));
+	// return (make_arr(matrix, p));
 }
